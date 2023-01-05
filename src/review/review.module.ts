731205cf14faedf3c6be2dbf412e8e2777c428ee
Nikita8121 <natanalegarh@gmail.com>
2023-01-05 13:58:26 +0200

    upd

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ReviewController } from './review.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ReviewService } from './review.service';
 
-@Module({})
+@Module({
+  controllers: [ReviewController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ReviewModule,
+        schemaOptions: {
+          collection: 'Review',
+        },
+      },
+    ]),
+  ],
+  providers: [ReviewService],
+})
 export class ReviewModule {}
